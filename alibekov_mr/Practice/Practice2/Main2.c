01331837455d71a1a93ae9f93fd97010ecc685b1
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2018-10-14 23:46:13 +0300

    Practice3

--- alibekov_mr/Practice/Practice2/Main2.c
+++ alibekov_mr/Practice/Practice2/Main2.c
@@ -1,66 +1,124 @@
 #include<stdio.h>
 void main() {
-	int i, n, game_over = 0, x = 78, x_forChanges = 78, min = 1, max, x_igroka_forChanges, x_igroka, b=0, k=0;
+	int i, j = 0, q = 0, n, game_over = 0, x = 0, x_forChanges, min = 1, max, x_igroka_forChanges, x_igroka, b = 0, k = 0;
 	short case0 = 0, case1 = 0, case2 = 0, case3 = 0, case4 = 0, case5 = 0, case6 = 0, case7 = 0, case8 = 0, case9 = 0;
 	do {
 		printf("Vvedite n\n");
 		scanf_s("%d", &n);
 	} while (n<=0);
-	for (i = 0; i < n; i++) { min *= 10; } //min znachenie chisla dlini n
+
+	for (i = 1; i < n; i++) { min *= 10; } //min znachenie chisla dlini n
 	max = min * 10;
-	while (game_over == 0) {
-		/* random  x */
 
-
+	scanf_s("%d", &x_igroka);
+	while ((x_igroka < min) || (x_igroka >= max)) {
+		printf("Vvedeno plohoe chislo\n");
 		scanf_s("%d", &x_igroka);
-		for (i = 0; i < n; i++) {  
-			switch (x_igroka_forChanges % 10)
-			{
-			case 0: case0++;
-			case 1: case1++;
-			case 2: case2++;
-			case 3: case3++;
-			case 4: case4++;
-			case 5: case5++;
-			case 6: case6++;
-			case 7: case7++;
-			case 8: case8++;
-			case 9: case9++;
-			}
-			x_igroka_forChanges /= 10;
+		
+	} 
+		x_igroka_forChanges = x_igroka;
+	for (i = 0; i < n; i++) {                        
+		while (j < (((((x_igroka_forChanges * (i + 1)) % 255) % 7) * n) % 10)) {
+			if (q < 10) q++;
+			else q = 1;
+			j++;
 		}
+		j = 0;
+		while (j < i) {
+			q *= 10;
+			j++;
+		}
+		x += q;
+		q = 0;
+	}
+
+	while (game_over == 0) {
+		
+		scanf_s("%d", &x_igroka);
 		while ((x_igroka < min) || (x_igroka >= max)) {
 			printf("Vvedeno plohoe chislo\n");
 			scanf_s("%d", &x_igroka);
 		}
+			printf("\n%d\n", x);
+
 		x_forChanges = x;
 		x_igroka_forChanges = x_igroka;
-		for (i = 0; i < n; i++) {                          //while (x_forChanges !=0 )
+		for (i = 1; i <= n; i++) {                          //while (x_forChanges !=0 )
 			if ((x_forChanges % 10) == (x_igroka_forChanges % 10)) b++;
 			x_forChanges /= 10;
 			x_igroka_forChanges /= 10;
 		}
-
+		x_forChanges = x;
+		x_igroka_forChanges = x_igroka;
 		printf("Kolichestvo bikov: %d\n", b);
 		
 		/* k = ?
-
 		for (i = 1; i <= n; i++){
 		cells_of_x[i] = x_forChanges % 10;                       //massiv iz zifr chisla komputera
 		cells_of_x_igroka[i] = x_igroka_forChanges % 10;         //massiv iz zifr chisla igroka
 		x_forChanges /= 10;
 		x_igroka_forChanges /= 10;
 		}
-		
-		
-		
 		*/
-
-
+		for (i = 1; i <= n; i++) {
+			switch (x_forChanges % 10) {
+			case 0: case0 = i;
+					break;
+			case 1: case1 = i;
+				break;
+			case 2: case2 = i;
+				break;
+			case 3: case3 = i;
+				break;
+			case 4: case4 = i;
+				break;
+			case 5: case5 = i;
+				break;
+			case 6: case6 = i;
+				break;
+			case 7: case7 = i;
+				break;
+			case 8: case8 = i;
+				break;
+			case 9: case9 = i;
+				break;
+			}
+			x_forChanges /= 10;
+		}
+		x_forChanges = x;
+		for (i = 1; i <= n; i++) {
+			switch (x_igroka_forChanges % 10){
+			case 0: if ((case0 != i) && (case0 != 0)) k++;
+				break;
+			case 1: if ((case1 != i) && (case1 != 0)) k++;
+				break;
+			case 2: if ((case2 != i) && (case2 != 0)) k++;
+				break;
+			case 3: if ((case3 != i) && (case3 != 0)) k++;
+				break;
+			case 4: if ((case4 != i) && (case4 != 0)) k++;
+				break;
+			case 5: if ((case5 != i) && (case5 != 0)) k++;
+				break;
+			case 6: if ((case6 != i) && (case6 != 0)) k++;
+				break;
+			case 7: if ((case7 != i) && (case7 != 0)) k++;
+				break;
+			case 8: if ((case8 != i) && (case8 != 0)) k++;
+				break;
+			case 9: if ((case9 != i) && (case9 != 0)) k++;
+				break;
+			}
+			x_igroka_forChanges /= 10;
+		}
+		x_igroka_forChanges = x_igroka;
 		printf("Kolichestvo korov: %d\n", k);
+
 		if (b == n){             //if (x = x_igroka) {
 			game_over = 1;
-			printf("You win");
+			printf("You win\n");
 		}
+		b = 0;
+		k = 0;
 	}
 }
