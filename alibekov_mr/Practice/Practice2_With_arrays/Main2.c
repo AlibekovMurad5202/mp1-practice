51b5a9ba0c4bebceb7d466348e654771a98a4e5e
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2018-10-20 22:58:00 +0300

    22:57

--- alibekov_mr/Practice/Practice2_With_arrays/Main2.c
+++ alibekov_mr/Practice/Practice2_With_arrays/Main2.c
@@ -1,30 +1,31 @@
 #define N 10
 #include<stdio.h>               //Скорость подбора x целиком
 #include<stdlib.h>				//и полностью зависит от текущего времени.
-#include<time.h>				//Программа работает быстро для n = 1,2,3,4.
+#include<time.h>				//Программа работает быстро для n = 1,2,3,4, но не для n > 4
 #include<stdbool.h>				//Нужно поменять способ подбора!
 #include<locale.h>				//(в частности, подбирать каждую цифру отдельно)
 #include<math.h>
 void main() {
-	int x_igroka, new_game,p, i, j = 0, steps = 0, n, x = 1, x_forChanges, min = 1, max, x_igroka_forChanges, b = 0, k = 0;
+	int x_igroka, new_game, p, i, vvedeno_pravilno = 1, j = 0, steps = 0, n, x = 1, x_forChanges, min = 1, max, x_igroka_forChanges, b = 0, k = 0;
 	short cells_x[10] = {0};//Создаем и обнуляем массив, в которых будут номера цифр в числе x 
 	short cells_x_igroka[10] = {0};//Создаем и обнуляем массив, в которых будут номера цифр в числе x_igroka
 	_Bool GameOver = false, xIsGood = true, x_igrokaIsGood = true;
-	//int number_x[N] = {0};
+	int number_x[N] = {0};
 
 	setlocale(LC_ALL, "Russian"); //Ура!!! Теперь можно говорить на русском языке
 
-	do
-	{
+	do {
 		do {                         //Ввод n + проверка
 			printf("Vvedite n\n");
-			scanf_s("%d", &n);
-		} while ((n <= 0) || (n > 9)); //Число n <= 9,т.к. иначе невозможно сгенерировать число с неповторяющимися цифрами
+			/*vvedeno_pravilno = */scanf_s("%d", &n);
+		} while ((n <= 0) || (n > 9) || (vvedeno_pravilno != 1)); //Число n <= 9,т.к. иначе невозможно сгенерировать число с неповторяющимися цифрами
 
 		for (i = 1; i < n; i++)  min *= 10;    //min znachenie chisla dlini n
 		max = min * 10;                        //max znachenie chisla dlini n
 
-		do  //цикл проверяющий число x на повторение цифр
+		//vvedeno_pravilno = 0;
+
+		/*do  //цикл проверяющий число x на повторение цифр
 		{
 			srand((unsigned int)time(0));                  //Random
 			
@@ -46,42 +47,65 @@ void main() {
 				}
 				x_forChanges /= 10; // Пример: число 196 -> cells_x[6]=1; cells_x[9]=2; cells_x[1]=3. Остальные ячейки равны 0.                
 			}
-		} while (!xIsGood);   //
+		} while (!xIsGood); */  //
 		 
-	
-/*
-		for (i = 0; i < n; i++) {
-			srand((unsigned int)time(0));
-			if (i != 0) number_x[i] = 8.0 / RAND_MAX * rand() + 1;
-			else number_x[i] = 8.0 / RAND_MAX * rand() + 1;
-		}
-		for (i = 0; i < n; i++)
-		{
-			j = 0;
-			while (i  > j)
-			{
-				number_x[i] *= 10;
-				j++;
+
+
+
+
+
+		j = 0;
+		i = 0;
+		//do {
+			while(i < n) {
+				srand((unsigned int)time(0));
+				if ((i == (n - 1)) && ((rand() % 10) == 0)) number_x[i] = 4;
+				else number_x[i] = rand() % 10;
+				for ( j = 0; j < i; j++) {
+					if (number_x[i] == number_x[j]) {
+						i--;
+						break;
+					};
+				}
+				printf("%d\n", i);
+				i++;
 			}
-		}
-
-		x = number_x[0];
-		for (i = 1; i < n; i++) {
-			//printf("%d\n", number_x[i]);
-			x += number_x[i];
-
-		}
+		//} while (!xIsGood);
 
 
-		*/
-		do
-		{
+		j = 0;
+
+					//printf("\n");
+			for (i = 0; i < n; i++) {
+				for ( j = 0; j < i; j++){
+					number_x[i] *= 10;
+				}
+				j = 0;
+				//printf("%d\n", number_x[i]);
+			}
+			for (i = 0; i < n; i++) {
+				x += number_x[i];
+			}
+			//printf("\n");
+			printf("%d\n", x);
+
+			x_forChanges = x;
+			for (i = 0; i < n; i++) {     //Заполнение массива. Значение элемента - номер позиции (с конца) цифры в числе          
+					cells_x[x_forChanges % 10] = i + 1; //номер элемента - цифра в числе.
+					x_forChanges /= 10;
+				}
+		
+		do {
 			steps++;
-			do  //цикл проверяющий число x_igroka на повторение цифр
-			{
+			do { //цикл проверяющий число x_igroka на повторение цифр
 				printf("%d\n", x);
 
-				scanf_s("%d", &x_igroka);
+				vvedeno_pravilno = scanf_s("%d", &x_igroka);
+			/*	while (vvedeno_pravilno != 1) {
+					printf("Vvedeno ne chislo! Vvedite chislo");
+					scanf_s("%d", &x_igroka);
+					vvedeno_pravilno = scanf_s("%d", &x_igroka);
+				}*/
 				x_igroka_forChanges = x_igroka;     //"Мальчик для битья"
 				for (i = 0; i < n; i++) {     //Заполнение массива. Значение элемента - номер позиции (с конца) цифры в числе. Hомер элемента - цифра в числе.         
 					if ((cells_x_igroka[x_igroka_forChanges % 10] == 0) && (x_igroka >= min) && (x_igroka < max)) {
@@ -128,8 +152,7 @@ void main() {
 			}
 
 		} while (b != n);
-		for (p = 0; p <N; p++)
-		{
+		for (p = 0; p <N; p++) {
 			cells_x[p] = 0;
 		}
 		x = 0;
@@ -139,8 +162,7 @@ void main() {
 		k = 0;
 		x_igroka = 0;
 		x_igroka_forChanges = 0;
-		for (p = 0; p <N; p++)
-		{
+		for (p = 0; p <N; p++) {
 			cells_x_igroka[p] = 0;
 		}
 		steps = 0;
