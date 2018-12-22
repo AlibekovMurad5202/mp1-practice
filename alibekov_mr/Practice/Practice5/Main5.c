7f986d01e13058730d0230b860fa3a75dbd1afab
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2018-12-22 17:07:55 +0300

    17:07

--- alibekov_mr/Practice/Practice5/Main5.c
+++ alibekov_mr/Practice/Practice5/Main5.c
@@ -1,13 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define N 10
-#define k 5
-  
-//int bm[N];
 
 void input_a(int a[], int n) {
   int i;
-  for (i = 0; i < n; i++) scanf("%d", &(a[i]));
+  for (i = 0; i < n; i++)
+    scanf("%d", &(a[i]));
 }
 
 void output_a(int a[], int n) {
@@ -61,74 +58,93 @@ void Bubble_Sort(int a[], int n) {
 }
 
 void Counting_Sort(int a[], int n) {
-  int i, j;
-  int count[k] = { 0 };
-  for (i = 0; i < n; i++) count[a[i]]++;
+  int i, j, k = 0;
+  k = a[0];
+  for (i = 1; i < n; i++)
+    if (a[i] > k)
+      k = a[i];
+  k++;
+  int *count = (int *)malloc(k * sizeof(int));
+  for (i = 0; i < k; i++) count[i] = 0;
+  for (i = 0; i < n; i++) {
+    *(count + a[i]) = *(count + a[i]) + 1;
+    count - a[i];
+  }
   int index = 0;
   for (j = 0; j < k; j++) {
     for (i = 0; i < count[j]; i++) {
-      a[index++] = j;
+      a[index] = j;
+      index++;
     }
+    count - j;
   }
 }
 
 void quick_split(int a[], int *i, int *j, int p) {
-    do {
-        while (a[(*i)] < p) (*i)++;
-        while (a[(*j)] > p) (*j)--;
-        if ((*i) <= (*j)) {
-            int tmp = a[(*i)];
-            a[(*i)] = a[(*j)];
-            a[(*j)] = tmp;
-        }
-    } while ((*i) < (*j));
+  do {
+    while (a[(*i)] < p) (*i)++;
+    while (a[(*j)] > p) (*j)--;
+    if ((*i) <= (*j)) {
+      int tmp = a[(*i)];
+      a[(*i)] = a[(*j)];
+      a[(*j)] = tmp;
+    }
+  } while ((*i) < (*j));
 }
 
 void Quick_Sort(int a[], int n1, int n2) {
-    int m = (n1 + n2) / 2;
-    int i = n1, j = n2;
-    quick_split(a, &i, &j, a[m]);
-    if (i > n1) Quick_Sort(a, n1, i - 1);
-    if (j < n2) Quick_Sort(a, j + 1, n2);
+  int m = (n1 + n2) / 2;
+  int i = n1, j = n2;
+  quick_split(a, &i, &j, a[m]);
+  if (i > n1) Quick_Sort(a, n1, i - 1);
+  if (j < n2) Quick_Sort(a, j + 1, n2);
 }
 
-/*
-void Merge(int *a, int size_a, int *b, int size_b, int *c) {
-  int i, j, s;
-  i = j = s = 0;
-  while ((i < size_a) && (j < size_b))
+void Merge(int *a, int left, int mid, int right) {
+  int i = 0, j = 0, k;
+  int *c = (int *)malloc((right - left) * sizeof(int));
+  while ((left + i < mid) && (mid + j < right))
   {
-    if (a[i] <= b[j]) c[s++] = a[i++];
-    else c[s++] = b[j++];
+    if (a[left + i] < a[mid + j]) {
+      c[i + j] = a[left + i];
+      i++;
+    }
+    else {
+      c[i + j] = a[mid + j];
+      j++;
+    }
   }
-  if (i == size_a)
-    while (j < size_b) c[s++] = b[j++];
-  if (j == size_b)
-    while (i < size_a) c[s++] = a[i++];
+  while (left + i < mid) {
+    c[i + j] = a[left + i];
+    i++;
+  }
+  while (mid + j < right) {
+    c[i + j] = a[mid + j];
+    j++;
+  }
+  for (k = 0; k < i + j; k++) a[left + k] = c[k];
+  free(c);
 }
 
 void Merge_Sort(int a[], int left, int right) {
   int mid, i;
-  //int *b = (int *)malloc(N * sizeof(int));
-
-  if (left <= right) return;
-  mid = (left + right) / 2;
+  if (left + 1 >= right) return;
+  mid = (right + left) / 2;
   Merge_Sort(a, left, mid);
-  Merge_Sort(a, mid + 1, right);
-  Merge(a + left, mid + left - 1, a + mid, right - mid + 1, bm);
- // return &(b[0]);
+  Merge_Sort(a, mid, right);
+  Merge(a, left, mid, right);
 }
-*/
+
 void main() {
-  int a[N] = { 0 };
-  int choose, i;
-//  int *bm;
+  int N, choose;
+  printf("Enter count of elements in array.\n");
+  scanf("%d", &N);
+  int *a = (int *)malloc(N * sizeof(int));
   printf("Enter array.\n");
   input_a(a, N);
   printf("Your array: ");
   output_a(a, N);
-  printf("\nChoose method of sort:\n1)Choose Sort\n2)Insert Sort\n3)Bubble Sort\n");
-  printf("4)Counting Sort\n5)Quick Sort\n6)Merge Sort\n");
+  printf("\nChoose method of sort:\n1)Choose Sort\n2)Insert Sort\n3)Bubble Sort\n4)Counting Sort\n5)Quick Sort\n6)Merge Sort\n");
   scanf("%d", &choose);
   switch (choose)
   {
@@ -142,12 +158,11 @@ void main() {
     break;
   case 5: Quick_Sort(a, 0, N - 1);
     break;
- /* case 6: Merge_Sort(a, 0, N - 1);
-    for (i = 0; i < N; i++) {
-      a[i] = bm[i];
-    }
-    break;*/
+  case 6: Merge_Sort(a, 0, N);
+    break;
   }
   printf("\n");
   output_a(a, N);
+
+  free(a);
 }
