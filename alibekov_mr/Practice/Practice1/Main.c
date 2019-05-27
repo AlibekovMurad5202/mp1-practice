f522e7adce946daf356bd7a7de0e5baaa0a50565
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2019-05-28 02:02:39 +0300

    new beginning

--- alibekov_mr/Practice/Practice1/Main.c
+++ alibekov_mr/Practice/Practice1/Main.c
@@ -1,20 +1,26 @@
 #include<stdio.h>
-void main() {
-	double pl_DVP, pl_DSP, pl_d, m, m_stenka, m_2bokovini, m_2krish, m_2dveri, m_polki;
-	int w, h, d;
-	printf("Vvedite (v sm): w (ot 80 do 120), h (ot 180 do 220), d (ot 50 do 90)\n");
-	scanf_s("%d %d %d", &w, &h, &d);
-	printf("Vvedite (v kg/(sm^3)): pl_DVP, pl_DSP, pl_d\n");
-	scanf_s("%lf %lf %lf", &pl_DVP, &pl_DSP, &pl_d);
-	if ((w < 80) || (w > 120) || (h < 180) || (h > 220) || (d < 50) || (d > 90) || (pl_DVP <= 0) || (pl_DSP <= 0) || (pl_d <= 0)) {
-		printf("Vvedeni nepravilnie dannnie\n");
-		return;
-	}
-	m_stenka = h * w * 0.5 * pl_DVP;
-	m_2bokovini = 2 * pl_DSP * h * d * 1.5;
-	m_2krish = 2 * pl_DSP * w * d * 1.5;
-	m_2dveri = h * w * 1 * pl_d;
-	m_polki = (h / 40) * pl_DSP * w * d * 1.5;
-	m = m_stenka + m_2bokovini + m_2krish + m_polki + m_2dveri;
-	printf("%lf\n", m);
+
+void main()
+{
+  double pl_DVP, pl_DSP, pl_d, m, m_stenka, m_2bokovini, m_2krishi, m_2dveri, m_polki;
+  double w, h, d;
+  printf("Vvedite (v sm): w (ot 80 do 120), h (ot 180 do 220), d (ot 50 do 90)\n");
+  scanf("%lf %lf %lf", &w, &h, &d);
+  printf("Vvedite (v kg/(m^3)): pl_DVP, pl_DSP, pl_d\n");
+  scanf("%lf %lf %lf", &pl_DVP, &pl_DSP, &pl_d);
+  if ((w < 80) || (w > 120) || (h < 180) || (h > 220) || (d < 50) || (d > 90) || (pl_DVP <= 0) || (pl_DSP <= 0) || (pl_d <= 0))
+  {
+    printf("Vvedeni nepravilnie parametri\n");
+    return;
+  }
+  w *= 0.01;
+  h *= 0.01;
+  d *= 0.01;
+  m_stenka = (h * w * 0.005) * pl_DVP;
+  m_2bokovini = 2 * (h * d * 0.015) * pl_DSP;
+  m_2krishi = 2 * (w * d * 0.015) * pl_DSP;
+  m_2dveri = 2 * (h * (w / 2) * 0.01) * pl_d;
+  m_polki = ((int)((h - 2 * 0.015) / (0.4 + 0.015)) * (w - 2 * 0.015) * (d - 0.005) * 0.015) * pl_DSP;
+  m = m_stenka + m_2bokovini + m_2krishi + m_polki + m_2dveri;
+  printf("%lf kg\n", m);
 }
