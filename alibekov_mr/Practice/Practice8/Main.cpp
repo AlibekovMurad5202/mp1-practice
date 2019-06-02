fa5c06df5745d48fafc158ba4b3fc2f870fc0fc3
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2019-06-03 01:41:44 +0300

    1:41

--- alibekov_mr/Practice/Practice8/Main.cpp
+++ alibekov_mr/Practice/Practice8/Main.cpp
@@ -81,77 +81,77 @@ int main()
             std::cout << "errorLine: " << e.errorLine() << std::endl;
             std::cout << "errorFile: " << e.errorFile() << std::endl;
         }
-        
-        m_arr[4] = m4 * m3;
+
+        m_arr[4] = m4 + 9.;
         std::cout << m_arr[4];
 
         try {
-            m_arr[5] = m3 * m1;
+            m_arr[5] = m0 + 1.;
         }
-        catch (ExceptionDifferentDimensions const& e) {
+        catch (ExceptionEmptyMatrix const& e)
+        {
+            std::cout << std::endl << e.what() << std::endl;
+            std::cout << "errorLine: " << e.errorLine() << std::endl;
+            std::cout << "errorFile: " << e.errorFile() << std::endl;
+        }
+
+        m_arr[6] = m4 - 9.;
+        std::cout << m_arr[6];
+
+        try {
+            m_arr[7] = m0 - 1.;
+        }
+        catch (ExceptionEmptyMatrix const& e)
+        {
+            std::cout << std::endl << e.what() << std::endl;
+            std::cout << "errorLine: " << e.errorLine() << std::endl;
+            std::cout << "errorFile: " << e.errorFile() << std::endl;
+        }
+
+        m_arr[8] = m4 * 9.;
+        std::cout << m_arr[8];
+
+        try {
+            m_arr[9] = m0 * 1.;
+        }
+        catch (ExceptionEmptyMatrix const& e)
+        {
+            std::cout << std::endl << e.what() << std::endl;
+            std::cout << "errorLine: " << e.errorLine() << std::endl;
+            std::cout << "errorFile: " << e.errorFile() << std::endl;
+        }
+
+        m_arr[10] = m4 * m3;
+        std::cout << m_arr[10];
+        
+        try {
+            m_arr[11] = m3 * m1;
+        }
+        catch (ExceptionUndefinedMultiplication const& e) {
             std::cout << std::endl << e.what() << std::endl;
             std::cout << "errorLine: " << e.errorLine() << std::endl;
             std::cout << "errorFile: " << e.errorFile() << std::endl;
         }
         
-        m_arr[6] = m4 + m5;
-        std::cout << m_arr[6];
-
-        try {
-            m_arr[7] = m3 + m1;
-        }
-        catch (ExceptionDifferentDimensions const& e) {
-            std::cout << std::endl << e.what() << std::endl;
-            std::cout << "errorLine: " << e.errorLine() << std::endl;
-            std::cout << "errorFile: " << e.errorFile() << std::endl;
-        }
-
-        m_arr[8] = m4 - m5;
-        std::cout << m_arr[8];
-
-        try {
-            m_arr[9] = m3 - m1;
-        }
-        catch (ExceptionDifferentDimensions const& e) {
-            std::cout << std::endl << e.what() << std::endl;
-            std::cout << "errorLine: " << e.errorLine() << std::endl;
-            std::cout << "errorFile: " << e.errorFile() << std::endl;
-        }
-
-        m_arr[10] = m4 + 9.;
-        std::cout << m_arr[10];
-
-        try {
-            m_arr[11] = m0 + 1.;
-        }
-        catch (ExceptionEmptyMatrix const& e)
-        {
-            std::cout << std::endl << e.what() << std::endl;
-            std::cout << "errorLine: " << e.errorLine() << std::endl;
-            std::cout << "errorFile: " << e.errorFile() << std::endl;
-        }
-
-        m_arr[12] = m4 - 9.;
+        m_arr[12] = m4 + m5;
         std::cout << m_arr[12];
 
         try {
-            m_arr[13] = m0 - 1.;
+            m_arr[13] = m3 + m1;
         }
-        catch (ExceptionEmptyMatrix const& e)
-        {
+        catch (ExceptionDifferentDimensions const& e) {
             std::cout << std::endl << e.what() << std::endl;
             std::cout << "errorLine: " << e.errorLine() << std::endl;
             std::cout << "errorFile: " << e.errorFile() << std::endl;
         }
 
-        m_arr[14] = m4 * 9.;
+        m_arr[14] = m4 - m5;
         std::cout << m_arr[14];
 
         try {
-            m_arr[15] = m0 * 1.;
+            m_arr[15] = m3 - m1;
         }
-        catch (ExceptionEmptyMatrix const& e)
-        {
+        catch (ExceptionDifferentDimensions const& e) {
             std::cout << std::endl << e.what() << std::endl;
             std::cout << "errorLine: " << e.errorLine() << std::endl;
             std::cout << "errorFile: " << e.errorFile() << std::endl;
