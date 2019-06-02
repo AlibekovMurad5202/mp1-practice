2652f187cc8f3b0cb8421a1b2cea36694c73f244
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2019-06-02 14:16:55 +0300

    14:16

--- alibekov_mr/Practice/Practice6/Main.c
+++ alibekov_mr/Practice/Practice6/Main.c
@@ -1,156 +1,156 @@
 #include "Vector.h"
 #include "stdio.h"
- 
+
 void main()
 {
-	vector *_vector1 = NULL;
-	vector *_vector2 = NULL;
-	vector *_vector3 = NULL;
-	vector *v_rezult = NULL;
+    vector* _vector1 = NULL;
+    vector* _vector2 = NULL;
+    vector* _vector3 = NULL;
+    vector* v_rezult = NULL;
 
-	int count1 = 0, count2 = 0, count3 = count2, i;
-	double _rezult = 0;
+    int count1 = 0, count2 = 0, count3 = count2, i;
+    double _rezult = 0;
 
-	/* Creating */
-	{
-		printf("Demension of the first vector: ");
-		scanf("%d", &count1);
-		if (CreateVector(&count1, &_vector1)) { system("pause"); return; }
+    /* Creating */
+    {
+        printf("Demension of the first vector: ");
+        scanf("%d", &count1);
+        if (CreateVector(&count1, &_vector1)) { system("pause"); return; }
 
-		printf("Demension of the second and the third vectors: ");
-		scanf("%d", &count2);
-		if (CreateVector(&count2, &_vector2)) { system("pause"); return; }
+        printf("Demension of the second and the third vectors: ");
+        scanf("%d", &count2);
+        if (CreateVector(&count2, &_vector2)) { system("pause"); return; }
 
-		count3 = count2;
-		if (CreateVector(&count3, &_vector3)) { system("pause"); return; }
-	}
+        count3 = count2;
+        if (CreateVector(&count3, &_vector3)) { system("pause"); return; }
+    }
 
 
-	printf("Enter the first vector: \n");
-	for (i = 0; i < count1; i++) scanf("%lf", &(_vector1->d)[i]);
-	printf("Enter the second vector: \n");
-	for (i = 0; i < count2; i++) scanf("%lf", &(_vector2->d)[i]);
-	printf("Enter the third vector: \n");
-	for (i = 0; i < count3; i++) scanf("%lf", &(_vector3->d)[i]);
-	
-	printf("\nThe first vector: (");
-	for (i = 0; i < _vector1->n; i++) printf(" %lf", (_vector1->d)[i]);
-	printf(" )\n");
+    printf("Enter the first vector: \n");
+    for (i = 0; i < count1; i++) scanf("%lf", &(_vector1->d)[i]);
+    printf("Enter the second vector: \n");
+    for (i = 0; i < count2; i++) scanf("%lf", &(_vector2->d)[i]);
+    printf("Enter the third vector: \n");
+    for (i = 0; i < count3; i++) scanf("%lf", &(_vector3->d)[i]);
 
-	printf("The second vector: (");
-	for (i = 0; i < _vector2->n; i++) printf(" %lf", (_vector2->d)[i]);
-	printf(" )\n");
+    printf("\nThe first vector: (");
+    for (i = 0; i < _vector1->n; i++) printf(" %lf", (_vector1->d)[i]);
+    printf(" )\n");
 
-	printf("The third vector: (");
-	for (i = 0; i < _vector3->n; i++) printf(" %lf", (_vector3->d)[i]);
-	printf(" )\n\n");
+    printf("The second vector: (");
+    for (i = 0; i < _vector2->n; i++) printf(" %lf", (_vector2->d)[i]);
+    printf(" )\n");
 
-	/* Angles */
-	{
-		if (Angle(_vector1, _vector2, &_rezult))
-			printf("That's why angle between the first and the second vectors cann't be found!\n");
-		else printf("Angle between the first and the second vectors: %lf\n", _rezult);
+    printf("The third vector: (");
+    for (i = 0; i < _vector3->n; i++) printf(" %lf", (_vector3->d)[i]);
+    printf(" )\n\n");
 
-		if (Angle(_vector2, _vector3, &_rezult))
-			printf("That's why angle between the second and the third vectors cann't be found!\n");
-		else printf("Angle between the second and the third vectors: %lf\n", _rezult);
+    /* Angles */
+    {
+        if (Angle(_vector1, _vector2, &_rezult))
+            printf("That's why angle between the first and the second vectors cann't be found!\n");
+        else printf("Angle between the first and the second vectors: %lf\n", _rezult);
 
-		if (Angle(_vector1, _vector3, &_rezult))
-			printf("That's why angle between the first and the third vectors cann't be found!\n\n");
-		else printf("Angle between the first and the third vectors: %lf\n\n", _rezult);
-	}   
+        if (Angle(_vector2, _vector3, &_rezult))
+            printf("That's why angle between the second and the third vectors cann't be found!\n");
+        else printf("Angle between the second and the third vectors: %lf\n", _rezult);
 
-	/* ScalarProducts */
-	{
-		if (ScalarProduct(_vector1, _vector2, &_rezult))
-			printf("That's why scalar product between the first and the second vectors cann't be found!\n");
-		else printf("ScalarProduct between the first and the second vectors: %lf\n", _rezult);
+        if (Angle(_vector1, _vector3, &_rezult))
+            printf("That's why angle between the first and the third vectors cann't be found!\n\n");
+        else printf("Angle between the first and the third vectors: %lf\n\n", _rezult);
+    }
 
-		if (ScalarProduct(_vector2, _vector3, &_rezult))
-			printf("That's why scalar product between the second and the third vectors cann't be found!\n");
-		else printf("ScalarProduct between the second and the third vectors: %lf\n", _rezult);
+    /* ScalarProducts */
+    {
+        if (ScalarProduct(_vector1, _vector2, &_rezult))
+            printf("That's why scalar product between the first and the second vectors cann't be found!\n");
+        else printf("ScalarProduct between the first and the second vectors: %lf\n", _rezult);
 
-		if (ScalarProduct(_vector1, _vector3, &_rezult))
-			printf("That's why scalar product between the first and the third vectors cann't be found!\n\n");
-		else printf("ScalarProduct between the first and the third vectors: %lf\n\n", _rezult);
-	}
+        if (ScalarProduct(_vector2, _vector3, &_rezult))
+            printf("That's why scalar product between the second and the third vectors cann't be found!\n");
+        else printf("ScalarProduct between the second and the third vectors: %lf\n", _rezult);
 
-	/* Lengths */
-	{
-		if (Length(_vector1, &_rezult))
-			printf("That's why length of the first vector cann't be found!\n");
-		else printf("Length of the first vector: %lf\n", _rezult);
+        if (ScalarProduct(_vector1, _vector3, &_rezult))
+            printf("That's why scalar product between the first and the third vectors cann't be found!\n\n");
+        else printf("ScalarProduct between the first and the third vectors: %lf\n\n", _rezult);
+    }
 
-		if (Length(_vector2, &_rezult))
-			printf("That's why length of the second vector cann't be found!\n");
-		else printf("Length of the second vector: %lf\n", _rezult);
+    /* Lengths */
+    {
+        if (Length(_vector1, &_rezult))
+            printf("That's why length of the first vector cann't be found!\n");
+        else printf("Length of the first vector: %lf\n", _rezult);
 
-		if (Length(_vector3, &_rezult))
-			printf("That's why length of the third vector cann't be found!\n\n");
-		else printf("Length of the third vector: %lf\n\n", _rezult);
-	}
+        if (Length(_vector2, &_rezult))
+            printf("That's why length of the second vector cann't be found!\n");
+        else printf("Length of the second vector: %lf\n", _rezult);
 
-	/* Additions */
-	{
-		if (Addition(_vector1, _vector2, &v_rezult))
-			printf("That's why summation of the first and the second vectors cann't be found!\n");
-		else {
-			printf("Summation of the first and the second vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n");
-		}
+        if (Length(_vector3, &_rezult))
+            printf("That's why length of the third vector cann't be found!\n\n");
+        else printf("Length of the third vector: %lf\n\n", _rezult);
+    }
 
-		if (Addition(_vector1, _vector3, &v_rezult))
-			printf("That's why summation of the first and the third vectors cann't be found!\n");
-		else {
-			printf("Summation of the first and the third vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n");
-		}
+    /* Additions */
+    {
+        if (Addition(_vector1, _vector2, &v_rezult))
+            printf("That's why summation of the first and the second vectors cann't be found!\n");
+        else {
+            printf("Summation of the first and the second vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n");
+        }
 
-		if (Addition(_vector2, _vector3, &v_rezult))
-			printf("That's why summation of the second and the third vectors cann't be found!\n\n");
-		else {
-			printf("Summation of the second and the third vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n\n");
-		}
-	}
+        if (Addition(_vector1, _vector3, &v_rezult))
+            printf("That's why summation of the first and the third vectors cann't be found!\n");
+        else {
+            printf("Summation of the first and the third vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n");
+        }
 
-	/* Subtractions */
-	{
-		if (Subtraction(_vector1, _vector2, &v_rezult))
-			printf("Subtraction of the first and the second vectors cann't be found!\n");
-		else {
-			printf("Subtraction of the first and the second vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n");
-		}
-		
-		if (Subtraction(_vector1, _vector3, &v_rezult))
-			printf("Subtraction of the first and the third vectors cann't be found!\n");
-		else {
-			printf("Subtraction of the first and the third vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n");
-		}
-		
-		if (Subtraction(_vector2, _vector3, &v_rezult))
-			printf("Subtraction of the second and the third vectors cann't be found!\n\n");
-		else {
-			printf("Subtraction of the second and the third vectors: (");
-			for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
-			printf(" )\n\n");
-		}
-	}
+        if (Addition(_vector2, _vector3, &v_rezult))
+            printf("That's why summation of the second and the third vectors cann't be found!\n\n");
+        else {
+            printf("Summation of the second and the third vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n\n");
+        }
+    }
 
-	/* Deleting */
-	{
-		DeleteVector(_vector1);
-		DeleteVector(_vector2);
-		DeleteVector(_vector3);
-		DeleteVector(v_rezult);
-	}
+    /* Subtractions */
+    {
+        if (Subtraction(_vector1, _vector2, &v_rezult))
+            printf("Subtraction of the first and the second vectors cann't be found!\n");
+        else {
+            printf("Subtraction of the first and the second vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n");
+        }
 
-	system("pause"); 
+        if (Subtraction(_vector1, _vector3, &v_rezult))
+            printf("Subtraction of the first and the third vectors cann't be found!\n");
+        else {
+            printf("Subtraction of the first and the third vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n");
+        }
+
+        if (Subtraction(_vector2, _vector3, &v_rezult))
+            printf("Subtraction of the second and the third vectors cann't be found!\n\n");
+        else {
+            printf("Subtraction of the second and the third vectors: (");
+            for (i = 0; i < v_rezult->n; i++) printf(" %lf", (v_rezult->d)[i]);
+            printf(" )\n\n");
+        }
+    }
+
+    /* Deleting */
+    {
+        DeleteVector(_vector1);
+        DeleteVector(_vector2);
+        DeleteVector(_vector3);
+        DeleteVector(v_rezult);
+    }
+
+    system("pause");
 }
--- alibekov_mr/Practice/Practice6/Vector.c
+++ alibekov_mr/Practice/Practice6/Vector.c
@@ -4,137 +4,137 @@
 #include "stdlib.h"
 #define PI acos(-1.)
 
-int CreateVector(int *_n, vector **_vector) 
+int CreateVector(int* _n, vector** _vector)
 {
-	*_vector = (vector *)malloc(sizeof(vector));
-	if (*_n <= 0) 
-	{
-		printf("Error: Dimension is or less than zero! ");
-		return DimensionIsOrLessThanZero;
-	}
-	(*_vector)->n = *_n;
-	(*_vector)->d = (double *)malloc(sizeof(double) * ((*_vector)->n));
-	return ItWorks;
+    *_vector = (vector*)malloc(sizeof(vector));
+    if (*_n <= 0)
+    {
+        printf("Error: Dimension is or less than zero! ");
+        return DimensionIsOrLessThanZero;
+    }
+    (*_vector)->n = *_n;
+    (*_vector)->d = (double*)malloc(sizeof(double) * ((*_vector)->n));
+    return ItWorks;
 }
 
-void DeleteVector(vector *_vector)
+void DeleteVector(vector* _vector)
 {
-	if (_vector != NULL) 
-	{
-		free(_vector->d);
-		_vector->n = 0;
-		free(_vector);
-	}
+    if (_vector != NULL)
+    {
+        free(_vector->d);
+        _vector->n = 0;
+        free(_vector);
+    }
 }
 
-int Addition(vector *v1, vector *v2, vector **rezult)
+int Addition(vector* v1, vector* v2, vector** rezult)
 {
-	if ((v1 == NULL) || (v2 == NULL)) 
-	{
-		printf("Error: Vector is empty! ");
-		return VectorIsEmpty;
-	}
-	
-	int i, _n = v1->n;
-	
-	if (v1->n != v2->n) 
-	{
-		printf("Error: Vectors have different dimensions! ");
-		return DifferentDimensions;
-	}
-	DeleteVector(*rezult);
-	CreateVector(&_n, rezult);
-	for (i = 0; i < v1->n; i++) 
-		((*rezult)->d)[i] = (v1->d)[i] + (v2->d)[i];
-	return ItWorks;
+    if ((v1 == NULL) || (v2 == NULL))
+    {
+        printf("Error: Vector is empty! ");
+        return VectorIsEmpty;
+    }
+
+    int i, _n = v1->n;
+
+    if (v1->n != v2->n)
+    {
+        printf("Error: Vectors have different dimensions! ");
+        return DifferentDimensions;
+    }
+    DeleteVector(*rezult);
+    CreateVector(&_n, rezult);
+    for (i = 0; i < v1->n; i++)
+        ((*rezult)->d)[i] = (v1->d)[i] + (v2->d)[i];
+    return ItWorks;
 }
 
-int Subtraction(vector *v1, vector *v2, vector **rezult)
+int Subtraction(vector * v1, vector * v2, vector * *rezult)
 {
-	if ((v1 == NULL) || (v2 == NULL)) 
-	{
-		printf("Error: Vector is empty! ");
-		return VectorIsEmpty;
-	}
-	
-	int i, _n = v1->n;
-	
-	if (v1->n != v2->n) 
-	{
-		printf("Error: Vectors have different dimensions! ");
-		return DifferentDimensions;
-	}
-	DeleteVector(*rezult);
-	CreateVector(&_n, rezult);
-	for (i = 0; i < v1->n; i++) 
-		((*rezult)->d)[i] = (v1->d)[i] - (v2->d)[i];
-	return ItWorks;
+    if ((v1 == NULL) || (v2 == NULL))
+    {
+        printf("Error: Vector is empty! ");
+        return VectorIsEmpty;
+    }
+
+    int i, _n = v1->n;
+
+    if (v1->n != v2->n)
+    {
+        printf("Error: Vectors have different dimensions! ");
+        return DifferentDimensions;
+    }
+    DeleteVector(*rezult);
+    CreateVector(&_n, rezult);
+    for (i = 0; i < v1->n; i++)
+        ((*rezult)->d)[i] = (v1->d)[i] - (v2->d)[i];
+    return ItWorks;
 }
 
-int Length(vector *_vector, double *_length) 
+int Length(vector * _vector, double* _length)
 {
-	if (_vector == NULL) 
-	{
-		printf("Error: Vector is empty! ");
-		return VectorIsEmpty;
-	}
-	
-	double l = 0;
-	*_length = 0;
-	int i, _n = _vector->n;
-	
-	for (i = 0; i < _n; i++) 
-		l += ((_vector->d)[i] * (_vector->d)[i]);
-	*_length = sqrt(l);
-	return ItWorks;
+    if (_vector == NULL)
+    {
+        printf("Error: Vector is empty! ");
+        return VectorIsEmpty;
+    }
+
+    double l = 0;
+    *_length = 0;
+    int i, _n = _vector->n;
+
+    for (i = 0; i < _n; i++)
+        l += ((_vector->d)[i] * (_vector->d)[i]);
+    *_length = sqrt(l);
+    return ItWorks;
 }
 
-int ScalarProduct(vector *v1, vector *v2, double *_scalarProduct) 
+int ScalarProduct(vector * v1, vector * v2, double* _scalarProduct)
 {
-	if ((v1 == NULL) || (v2 == NULL)) 
-	{
-		printf("Error: Vector is empty! ");
-		return VectorIsEmpty;
-	}
-	
-	int i, _n = v1->n;
-	*_scalarProduct = 0;
-	
-	if (v1->n != v2->n) 
-	{
-		printf("Error: Vectors have different dimensions! ");
-		return DifferentDimensions;
-	}
-	for (i = 0; i < _n; i++) 
-		*_scalarProduct += ((v1->d)[i] * (v2->d)[i]);
-	return ItWorks;
+    if ((v1 == NULL) || (v2 == NULL))
+    {
+        printf("Error: Vector is empty! ");
+        return VectorIsEmpty;
+    }
+
+    int i, _n = v1->n;
+    *_scalarProduct = 0;
+
+    if (v1->n != v2->n)
+    {
+        printf("Error: Vectors have different dimensions! ");
+        return DifferentDimensions;
+    }
+    for (i = 0; i < _n; i++)
+        * _scalarProduct += ((v1->d)[i] * (v2->d)[i]);
+    return ItWorks;
 }
 
-int Angle(vector *v1, vector *v2, double *_rezultInGrad) 
+int Angle(vector * v1, vector * v2, double* _rezultInGrad)
 {
-    if ((v1 == NULL) || (v2 == NULL)) 
-	{
-		printf("Error: Vector is empty! ");
-		return VectorIsEmpty;
-	}
+    if ((v1 == NULL) || (v2 == NULL))
+    {
+        printf("Error: Vector is empty! ");
+        return VectorIsEmpty;
+    }
 
-	double l1, l2;
-	int codeOfError = 0;
-	double rezultInRad;
-	double _scalarProduct = 0;
+    double l1, l2;
+    int codeOfError = 0;
+    double rezultInRad;
+    double _scalarProduct = 0;
 
-	codeOfError = Length(v1, &l1);
-	if (codeOfError) return codeOfError;
-	codeOfError = Length(v2, &l2);
-	if (codeOfError) return codeOfError;
-	if ((l1 == 0) || (l2 == 0)) 
-	{
-		printf("Error: Length of vector is zero! ");
-		return LengthIsZero;
-	}	
-	codeOfError = ScalarProduct(v1, v2, &_scalarProduct);
-	if (codeOfError) return codeOfError;
-	rezultInRad = acos(_scalarProduct / (l1 * l2));
-	*_rezultInGrad = 180 * rezultInRad / PI;
-	return ItWorks;
+    codeOfError = Length(v1, &l1);
+    if (codeOfError) return codeOfError;
+    codeOfError = Length(v2, &l2);
+    if (codeOfError) return codeOfError;
+    if ((l1 == 0) || (l2 == 0))
+    {
+        printf("Error: Length of vector is zero! ");
+        return LengthIsZero;
+    }
+    codeOfError = ScalarProduct(v1, v2, &_scalarProduct);
+    if (codeOfError) return codeOfError;
+    rezultInRad = acos(_scalarProduct / (l1 * l2));
+    *_rezultInGrad = 180 * rezultInRad / PI;
+    return ItWorks;
 };
