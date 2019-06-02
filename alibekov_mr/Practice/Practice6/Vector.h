28cbc38b6de57ee016b6fdbdaea4439a2ec23e83
AlibekovMurad5202 <43135910+AlibekovMurad5202@users.noreply.github.com>
2019-06-02 14:18:51 +0300

    Update Vector.h

--- alibekov_mr/Practice/Practice6/Vector.h
+++ alibekov_mr/Practice/Practice6/Vector.h
@@ -2,32 +2,32 @@
 #define __VECTOR_H__
 
 typedef struct {
-	int n;
-	double *d;
+    int n;
+    double* d;
 } vector;
 
 enum Errors
 {
-	ItWorks,                    //0
-	DifferentDimensions,        //1
-	DimensionIsOrLessThanZero,  //2
-	VectorIsEmpty,              //3
-	LengthIsZero                //4
+    ItWorks,                    //0
+    DifferentDimensions,        //1
+    DimensionIsOrLessThanZero,  //2
+    VectorIsEmpty,              //3
+    LengthIsZero                //4
 };
 
 
-int CreateVector(int *_n, vector **_vector);
+int CreateVector(int* _n, vector** _vector);
 
-void DeleteVector(vector *_vector);
+void DeleteVector(vector* _vector);
 
-int Addition(vector *v1, vector *v2, vector **rezult);
+int Addition(vector* v1, vector* v2, vector** rezult);
 
-int Subtraction(vector *v1, vector *v2, vector **rezult);
+int Subtraction(vector* v1, vector* v2, vector** rezult);
 
-int Length(vector *_vector, double *_length);
+int Length(vector* _vector, double* _length);
 
-int ScalarProduct(vector *v1, vector *v2, double *_scalarProduct);
+int ScalarProduct(vector* v1, vector* v2, double* _scalarProduct);
 
-int Angle(vector *v1, vector *v2, double *_rezultInGrad);
+int Angle(vector* v1, vector* v2, double* _rezultInGrad);
 
 #endif // !__VECTOR_H__
