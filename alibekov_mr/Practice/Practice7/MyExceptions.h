6d101a0dd420becc3c456f43a7b5bc3c01a72830
AlibekovMurad5202 <muradalibekov2000@gmail.com>
2019-06-03 05:58:47 +0300

    5:58

--- alibekov_mr/Practice/Practice7/MyExceptions.h
+++ alibekov_mr/Practice/Practice7/MyExceptions.h
@@ -3,69 +3,69 @@
 
 #include <exception> 
 
-class MyException : public std::exception
+class MyException : std::exception
 {
-protected: 
-  char s[20] = "Unknown exception!";
-  const char* str_what;
-  int line;
-  const char* file;
+protected:
+    char s[20] = "Unknown exception!";
+    const char* str_what;
+    int line;
+    const char* file;
 
 public:
-  MyException();
-  MyException(int _line, const char *_file);
-  MyException(const MyException& _exception);
-  MyException(const char* message, int _line, const char *_file);
-  ~MyException();
+    MyException();
+    MyException(int _line, const char* _file);
+    MyException(const MyException& _exception);
+    MyException(const char* message, int _line, const char* _file);
+    ~MyException();
 
-  virtual const char* what() const { return str_what ? str_what : s;  }
-  virtual int errorLine() const { return line; }
-  virtual const char* errorFile() const { return file; }
+    virtual const char* what() const { return str_what ? str_what : s; }
+    virtual int errorLine() const { return line; }
+    virtual const char* errorFile() const { return file; }
 };
 
-class ExceptionOutOfRange : public MyException 
+class ExceptionOutOfRange : public MyException
 {
 public:
-  ExceptionOutOfRange();
-  ExceptionOutOfRange(const ExceptionOutOfRange& _exception);
-  ExceptionOutOfRange(int _line, const char *_file);
-  ~ExceptionOutOfRange();
+    ExceptionOutOfRange();
+    ExceptionOutOfRange(const ExceptionOutOfRange& _exception);
+    ExceptionOutOfRange(int _line, const char* _file);
+    ~ExceptionOutOfRange();
 };
 
 class ExceptionDifferentDimensions : public MyException
 {
 public:
-  ExceptionDifferentDimensions();
-  ExceptionDifferentDimensions(const ExceptionDifferentDimensions& _exception);
-  ExceptionDifferentDimensions(int _line, const char *_file);
-  ~ExceptionDifferentDimensions();
+    ExceptionDifferentDimensions();
+    ExceptionDifferentDimensions(const ExceptionDifferentDimensions& _exception);
+    ExceptionDifferentDimensions(int _line, const char* _file);
+    ~ExceptionDifferentDimensions();
 };
 
 class ExceptionBadAlloc : public MyException
 {
 public:
-  ExceptionBadAlloc();
-  ExceptionBadAlloc(const ExceptionBadAlloc& _exception);
-  ExceptionBadAlloc(int _line, const char *_file);
-  ~ExceptionBadAlloc();
+    ExceptionBadAlloc();
+    ExceptionBadAlloc(const ExceptionBadAlloc& _exception);
+    ExceptionBadAlloc(int _line, const char* _file);
+    ~ExceptionBadAlloc();
 };
 
 class ExceptionEmptyVector : public MyException
 {
 public:
-  ExceptionEmptyVector();
-  ExceptionEmptyVector(const ExceptionEmptyVector& _exception);
-  ExceptionEmptyVector(int _line, const char *_file);
-  ~ExceptionEmptyVector();
+    ExceptionEmptyVector();
+    ExceptionEmptyVector(const ExceptionEmptyVector& _exception);
+    ExceptionEmptyVector(int _line, const char* _file);
+    ~ExceptionEmptyVector();
 };
 
 class ExceptionNotPositiveDimension : public MyException
 {
 public:
-  ExceptionNotPositiveDimension();
-  ExceptionNotPositiveDimension(const ExceptionNotPositiveDimension& _exception);
-  ExceptionNotPositiveDimension(int _line, const char *_file);
-  ~ExceptionNotPositiveDimension();
+    ExceptionNotPositiveDimension();
+    ExceptionNotPositiveDimension(const ExceptionNotPositiveDimension& _exception);
+    ExceptionNotPositiveDimension(int _line, const char* _file);
+    ~ExceptionNotPositiveDimension();
 };
 
 #endif // !__MY_EXCEPTIONS_H__
--- alibekov_mr/Practice/Practice8/Matrix.h
+++ alibekov_mr/Practice/Practice8/Matrix.h
@@ -6,35 +6,30 @@
 
 class Matrix
 {
-public:
-  Matrix();
-  Matrix(int _rows, int _columns);
-  Matrix(int _rows, int _columns, double * _cells);
-  Matrix(const Matrix & _matrix);
-
-  ~Matrix();
-
-  Matrix operator+ (const Matrix & _matrix) const;
-  Matrix operator+ (double d) const;
-  Matrix operator- (const Matrix & _matrix) const;
-  Matrix operator- (double d) const;
-  Matrix operator* (const Matrix & _matrix) const;
-  Matrix operator* (double d) const;
-  
-  const Matrix& operator= (const Matrix & _matrix);
-
-  const double * operator[] (int numberOfRows) const;
-
-  friend std::istream & operator>> (std::istream & in, Matrix & _matrix);
-  friend std::ostream & operator<< (std::ostream & out, Matrix & _matrix);
-
 private:
-  int rows;
-  int columns;
+    int rows, columns;
+    double* cells;
 
-  double *cells;
+public:
+    Matrix();
+    Matrix(int _rows, int _columns);
+    Matrix(int _rows, int _columns, double* _cells);
+    Matrix(const Matrix& _matrix);
+    ~Matrix();
+
+    Matrix operator+ (const Matrix& _matrix) const;
+    Matrix operator+ (double d) const;
+    Matrix operator- (const Matrix& _matrix) const;
+    Matrix operator- (double d) const;
+    Matrix operator* (const Matrix& _matrix) const;
+    Matrix operator* (double d) const;
+
+    const Matrix& operator= (const Matrix& _matrix);
+
+    const double* operator[] (int numberOfRows) const;
+
+    friend std::istream& operator>> (std::istream& in, Matrix& _matrix);
+    friend std::ostream& operator<< (std::ostream& out, Matrix& _matrix);
 };
-
-
 
 #endif // !__MATRIX__H__
